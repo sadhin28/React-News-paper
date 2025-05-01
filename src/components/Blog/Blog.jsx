f0074a81a364128e2ac4cbe007510101c094ea14
sadhin28 <taosifsadhin113@gmail.com>
2025-05-02 02:38:53 +0600

    add font awsom

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,30 @@
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {bookmark} from "@fortawesome/free-solid-svg-icons";
 const Blog = ({blog}) => {
     
-   const {title}=blog
+   const {title,img,cover,reading_time}=blog
     return (
         <div>
-            <h2 className="text-2xl">Title:{title}</h2>
+           <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src={cover}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      {title}
+     <button className="btn">{reading_time} min read <FontAwesomeIcon icon={bookmark} /></button>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline">Fashion</div>
+      <div class="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
+
         </div>
     );
 };
