b66144df645d5548153bc9f3ec5a99ca10c8307e
sadhin28 <taosifsadhin113@gmail.com>
2025-05-01 23:42:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './header/Nav'
+function App() {
+  return (
+    <>
+      <div>
+          <Nav></Nav>
+      </div>
+       <h1>New Project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/header/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+
+        <div className=" p-5 navbar bg-base-100 shadow-sm flex justify-between">
+            <h1 className="font-bold text-xl lg:text-3xl md:text-2xl">Knowledge Cafe</h1>
+            <div className="avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+                    </div>
+                </div>
+        
+        </div>
+
+    )
+}
+export default Nav
