6c7fb23999501a64f491bcad856e2a0988163dfe
sadhin28 <taosifsadhin113@gmail.com>
2025-05-02 04:06:27 +0600

    add margint to

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav() {
     return (
 
-        <div className=" p-5 navbar bg-base-100 shadow-sm flex justify-between">
+        <div className=" fixed top-0 z-60 p-5 navbar bg-base-100 shadow-sm flex justify-between">
             <h1 className="font-bold text-xl lg:text-3xl md:text-2xl">Knowledge Cafe</h1>
             <div className="avatar">
                     <div className="w-10 rounded-full">
