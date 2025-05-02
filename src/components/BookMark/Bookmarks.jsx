dbd03a77b7f47fe641fa22cbe26037ef77e0089b
sadhin28 <taosifsadhin113@gmail.com>
2025-05-02 22:47:10 +0600

    add bookmark data

--- src/components/BookMark/Bookmarks.jsx
+++ src/components/BookMark/Bookmarks.jsx
@@ -3,7 +3,7 @@ const Bookmarks = ({bkMarks}) => {
 
     return (
         <div className="md:w-1/3">
-          <h2>BookMarks: {bkMarks.length}</h2>
+          <h2 className="text-center lg:text-3xl md:text-2xl text-xl font-bold">BookMarks: {bkMarks.length}</h2>
             
         </div>
     );
