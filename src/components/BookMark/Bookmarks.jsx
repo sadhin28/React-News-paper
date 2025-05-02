3053c8a0021bd3d0dd9c756b7a5273959ebfb2fc
sadhin28 <taosifsadhin113@gmail.com>
2025-05-03 02:04:02 +0600

    add reading time

--- src/components/BookMark/Bookmarks.jsx
+++ src/components/BookMark/Bookmarks.jsx
@@ -1,13 +1,13 @@
 
 import PropTypes from 'prop-types'
 import Book from '../Book/Book';
-const Bookmarks = ({bkMarks}) => {
+const Bookmarks = ({bkMarks,readingTime}) => {
     
     return (
         <div className="mt-14 bg-[#1111110D] md:w-1/3 mx-auto px-8 lg:px-0 md:px-0">
             <div>
             <div className=' border-b-blue-700 border rounded-xl px-8  bg-[#6047EC1A] h-20'>
-              <h4 className='text-center font-bold text-xl lg:text-2xl '><small>Spent time on read :  min</small></h4>
+              <h4 className='text-center font-bold text-xl lg:text-2xl '><small>Spent time on read : {readingTime}  min</small></h4>
             </div>
             </div>
             <div className='px-8 mt-2  py-2 rounded-xl'>
