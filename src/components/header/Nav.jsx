05a8a78e6efcfcf0751c07991746261a1408939a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-02 23:39:14 +0600

    add header backround color

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav() {
     return (
 
-        <div className="max-w-7xl mx-auto fixed top-0 z-60 navbar bg-base-100 shadow-sm flex justify-between h-25">
+        <div className="max-w-7xl mx-auto fixed top-0 z-60 navbar  shadow-sm flex justify-between h-25 bg-white text-black">
             <h1 className="font-bold text-xl lg:text-3xl md:text-2xl">Knowledge Cafe</h1>
             <div className="avatar">
                     <div className="w-10 rounded-full">
