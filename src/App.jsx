fc49ae127941d3f46f2c478797bd73bc4ea23590
sadhin28 <taosifsadhin113@gmail.com>
2025-05-02 21:14:19 +0600

    add bacround color

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Bookmarks from './components/BookMark/Bookmarks'
 function App() {
 
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div className='max-w-7xl mx-auto '>
       <div>
         {/* Header Component */}
            <div>
