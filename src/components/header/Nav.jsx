d17393faaaa0bd83d7e283e238d72ca912498a36
sadhin28 <taosifsadhin113@gmail.com>
2025-05-04 00:54:59 +0600

    add profile picture

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -3,11 +3,11 @@ function Nav() {
 
         <div className="max-w-7xl mx-auto fixed top-0 z-60 navbar  shadow-sm flex justify-between h-25 bg-white text-black">
             <h1 className="font-bold text-xl lg:text-3xl md:text-2xl">Knowledge Cafe</h1>
-            <div className="avatar">
+           
                     <div className="w-10 rounded-full">
                        <img src="/src/images/profile.png" alt="com" />
                     </div>
-                </div>
+                
         
         </div>
 
