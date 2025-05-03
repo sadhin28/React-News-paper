430260cbba4612491af9b36eabbc19448b6c839d
sadhin28 <taosifsadhin113@gmail.com>
2025-05-04 01:47:51 +0600

    add profile picture from facebook

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Nav from './components/header/Nav'
 import Bookmarks from './components/BookMark/Bookmarks'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
     const [bkMarks,setBookmarks]=useState([])
@@ -20,12 +20,22 @@ function App() {
 
     }
 
+  const [profiles,setprofiles]=useState(arguments)
+
+   useEffect(()=>{
+      fetch('profile.json')
+      .then(res=>res.json())
+      .then(data=>{
+        setprofiles(data)
+      }
+    )
+   },[])
   return (
     <div className='max-w-7xl mx-auto '>
       <div >
         {/* Header Component */}
            <div>
-             <Nav></Nav>
+             <Nav profiles={profiles}></Nav>
            </div>
          {/* Blogs Component */}
         <div className='md:flex gap-10 mt-30 justify-between '>
--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,10 +1,20 @@
-function Nav() {
+import { useEffect } from "react"
+
+
+function Nav({profiles}) {
+    const {profile}=profiles
+   
     return (
 
         <div className="max-w-7xl mx-auto fixed top-0 z-60 navbar  shadow-sm flex justify-between h-25 bg-white text-black">
             <h1 className="font-bold text-xl lg:text-3xl md:text-2xl">Knowledge Cafe</h1>
            
-        <img src="/src/images/profile.png" alt="com" />
+            <div class="avatar avatar-placeholder">
+        <div class="bg-neutral text-neutral-content w-12 rounded-full">
+         <img className="" src={profile} alt="" />
+          </div>
+         </div>
+            
   
         </div>
 
