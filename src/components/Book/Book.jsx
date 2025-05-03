1854f7be5cb21cca275abd1993c69fc9cb563ee4
sadhin28 <taosifsadhin113@gmail.com>
2025-05-03 22:18:59 +0600

    add button

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -7,7 +7,7 @@ const Book = ({books}) => {
         <div class=" card bg-base-100 w-full shadow-sm border border-amber-400 mt-7 mb-7">
         <div class="card-body ">
           <h2 class="card-title">{title}</h2>
-          
+          <button className='btn hover:bg-black hover:text-white'>Delate</button>
         </div>
       </div>
     );
